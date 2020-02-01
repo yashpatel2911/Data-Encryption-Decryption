9b91380da222e7479ea0ac279da446051abc1be4
Yash Patel <yshp1997@gmail.com>
2020-01-31 19:49:51 -0500

    Exception Class

---
+++ Data-Encryption-Decryption/src/com/team6/Encryption_Decryption_Exception.java
@@ -0,0 +1,14 @@
+package com.team6;
+
+//reference: https://www.codejava.net/coding/file-encryption-and-decryption-simple-example
+
+public class Encryption_Decryption_Exception extends Exception
+{
+	public Encryption_Decryption_Exception() {
+    }
+ 
+    public Encryption_Decryption_Exception(String message, Throwable throwable)
+    {
+        super(message, throwable);
+    }
+}
